ca902accc2f24c99722147359fdb4ac4a38fdabf
Cedric Ziel <cedric.ziel@instana.com>
2019-10-31 16:09:14 +0100

    Add Instana EUM page transitions by subscribing to Angular router events
    Example: https://github.com/instana/website-monitoring-examples/commit/54176447b856769f044dc3341a4b98cbd3bc725e#diff-9d00cf67e0982b37a0eab7e3e781c72d

--- src/Web/WebSPA/Client/modules/app.component.ts
+++ src/Web/WebSPA/Client/modules/app.component.ts
@@ -7,6 +7,8 @@ import { ConfigurationService } from './shared/services/configuration.service';
 import { SignalrService } from './shared/services/signalr.service';
 import { ToastrService } from 'ngx-toastr';
 
+import { Router, ActivationEnd, NavigationEnd } from '@angular/router';
+
 /*
  * App Component
  * Top Level Component
@@ -25,7 +27,8 @@ export class AppComponent implements OnInit {
         private configurationService: ConfigurationService,
         private signalrService: SignalrService,
         private toastr: ToastrService,
-        vcr: ViewContainerRef
+        vcr: ViewContainerRef,
+        private router: Router
     ) {
         // TODO: Set Taster Root (Overlay) container
         //this.toastr.setRootViewContainerRef(vcr);
@@ -39,6 +42,26 @@ export class AppComponent implements OnInit {
         //Get configuration from server environment variables:
         console.log('configuration');
         this.configurationService.load();        
+
+        // Sniff page transitions for instana
+        let pageName;
+        this.router.events.subscribe(event => {
+            // Identifies the name of the page. The name of the page
+            // is unfortunately not available on the NavigationEnd event, so we have to
+            // get it from the the ActivationEnd. The ActivationEnd is the last
+            // event in the router chain for which an ActivatedRouteSnapshot is
+            // available.
+            if (event instanceof ActivationEnd) {
+                pageName = undefined;
+                if (event.snapshot && event.snapshot.routeConfig && event.snapshot.routeConfig.path) {
+                    pageName = event.snapshot.routeConfig.path;
+                }
+            // Once the navigation finished, report the page name to Instana.
+            } else if (event instanceof NavigationEnd && typeof ineum !== 'undefined') {
+                console.log('Set page to', pageName);
+                ineum('page', pageName);
+            }
+        });
     }
 
     public setTitle(newTitle: string) {
